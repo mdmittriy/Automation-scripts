a817c91364cc0bf2107f228a5cfa40b323af48a3
mdmittriy <mdmittriy@mentorchains.com>
2020-07-10 19:21:20 +0300

    strange error check

--- pattern_matching.py
+++ pattern_matching.py
@@ -3,3 +3,4 @@ import re
 print("Hello, world!")
 print("Test message that show that I can merge two branches")
 print("One more test in branch testing my changes username and password")
+print("one more check")
