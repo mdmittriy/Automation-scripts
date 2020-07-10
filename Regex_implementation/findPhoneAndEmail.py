6281737955c04abb4e38ae2e6cd310dc187c4007
mdmittriy <mdmittriy@mentorchains.com>
2020-07-11 00:52:28 +0300

    completed all files in Regex_inplementation folder

--- Regex_implementation/findPhoneAndEmail.py
+++ Regex_implementation/findPhoneAndEmail.py
@@ -27,11 +27,17 @@ text = str(pyperclip.paste())
 
 matches = []
 
+for groups in phoneRegex.findall(text):
+    phoneNum = "-".join([groups[1], groups[3], groups[5]])
+    if groups[8] != "":
+        phoneNum += "x" + groups[8]
+    matches.append(phoneNum)
 
-allPhones = phoneRegex.findall(text)
-allEmails = emailRegex.findall(text)
-for email in allEmails:
-    print(email[0])
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
-print(allEmails)
-
+if len(matches) > 0:
+    pyperclip.copy("\n".join(matches))
+    print("\n".join(matches))
+else:
+    print("Not found")
--- Regex_implementation/pattern_matching.py
+++ Regex_implementation/pattern_matching.py
@@ -79,4 +79,3 @@ print(
         "12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge"
     )
 )
-
